e3841715748e6f638a528dfe8048866ae9f60fea
Luan Bernardo <bernardo4gacl@gmail.com>
2024-07-25 20:28:13 -0300

    Otimização do build frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,32 +1,24 @@
+import express from "express";
+import cors from "cors";
 import gracefulShutdown from "http-graceful-shutdown";
 import app from "./app";
 import { initIO } from "./libs/socket";
 import { logger } from "./utils/logger";
 import { StartAllWhatsAppsSessions } from "./services/WbotServices/StartAllWhatsAppsSessions";
-import cors from 'cors';
 
-// Configurar o CORS para permitir requisições de fenix.ticket
+// Configurações do CORS
 const corsOptions = {
-  origin: ["http://fenix.ticket", "http://localhost:3000"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
+  origin: ["http://localhost:3000", "http://fenix.ticket:3000"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: ["Content-Type", "Authorization"]
 };
 
 app.use(cors(corsOptions));
 
-// Usa a porta definida no .env, se existir
-const port = process.env.PORT || 8081;
-
-const server = app.listen(port, () => {
-  logger.info(`Server started on port: ${port}`);
+const server = app.listen(process.env.PORT || 8080, () => {
+  logger.info(`Server started on port: ${process.env.PORT || 8080}`);
 });
 
-// Inicializa as conexões do Socket.IO
 initIO(server);
-
-// Inicia todas as sessões do WhatsApp
 StartAllWhatsAppsSessions();
-
-// Configura o desligamento gracioso do servidor
 gracefulShutdown(server);
