4533701668b9fef268410bf5aa7d545b5e527f9d
Luan Bernardo <bernardo4gacl@gmail.com>
2024-07-25 19:15:01 -0300

    Inserção do CORS para local

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,11 +3,30 @@ import app from "./app";
 import { initIO } from "./libs/socket";
 import { logger } from "./utils/logger";
 import { StartAllWhatsAppsSessions } from "./services/WbotServices/StartAllWhatsAppsSessions";
+import cors from 'cors';
 
-const server = app.listen(process.env.PORT, () => {
-  logger.info(`Server started on port: ${process.env.PORT}`);
+// Configurar o CORS para permitir requisições de fenix.ticket
+const corsOptions = {
+  origin: ["http://fenix.ticket", "http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+// Usa a porta definida no .env, se existir
+const port = process.env.PORT || 8081;
+
+const server = app.listen(port, () => {
+  logger.info(`Server started on port: ${port}`);
 });
 
+// Inicializa as conexões do Socket.IO
 initIO(server);
+
+// Inicia todas as sessões do WhatsApp
 StartAllWhatsAppsSessions();
+
+// Configura o desligamento gracioso do servidor
 gracefulShutdown(server);
